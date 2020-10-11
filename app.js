9bdee83c0d9af26d4364253c98c7ba23c8824e20
Aaron A. Abreu <a.modernfossil@gmail.com>
2020-10-11 09:08:32 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const quote = document.getElementById('text');
+const author = document.getElementById('author');
+
+const newQuoteBtn = document.getElementById('new-quote');
+
+let quotesData;
+let currentQuote = '';
+let currentAuthor = '';
+
+function getRandomQuote() {
+  fetch("https://type.fit/api/quotes")
+    .then((response) => response.json())
+    .then((data) => {
+      currentQuote = data[Math.floor(Math.random() * data.length)].text;
+      currentAuthor = data[Math.floor(Math.random() * data.length)].author;
+
+      console.log(currentQuote, currentAuthor);
+      quote.innerText = currentQuote;
+      author.innerText = currentAuthor;
+    });
+}
+
+function getQuote() {
+  getRandomQuote();
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+    encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+}
+
+
+// Event listener
+newQuoteBtn.addEventListener('click', getQuote);
